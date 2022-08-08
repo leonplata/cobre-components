c1d51b8803deb302c0a702b6f97403338fa5116a
Leon Plata <pcg.leon@outlook.com>
2022-08-08 09:19:29 -0400

    Migrated collapse to lit

---
+++ packages/collapse/src/oneup-collapse.base.ts
@@ -0,0 +1,49 @@
+import { LitElement, html, PropertyValueMap } from 'lit';
+import { property } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+import "@polymer/iron-collapse/iron-collapse.js";
+import "@polymer/iron-icon/iron-icon.js";
+import "@polymer/iron-icons/hardware-icons.js";
+
+export class OneupCollapseBase extends LitElement {
+
+  @property({ type: String, reflect: true, attribute: 'title' })
+  title = '';
+
+  @property({ type: Boolean, reflect: true, attribute: 'opened' })
+  opened = false;
+
+  protected willUpdate(changedProperties: PropertyValueMap<OneupCollapseBase> | Map<PropertyKey, unknown>): void {
+    if (changedProperties.has('opened')) {
+      const detail = { value: this.opened };
+      const event = new CustomEvent('opened-changed', { detail });
+      this.dispatchEvent(event);
+    }
+  }
+
+  protected toggle() {
+    this.opened = !this.opened;
+  }
+
+  render() {
+    return html`
+      <div
+        class="collapse-bar"
+        @click=${() => this.toggle()}
+      >
+        <iron-icon
+          class=${classMap({
+            ['collapsed-' + this.opened]: true
+          })}
+          icon="hardware:keyboard-arrow-down"
+        ></iron-icon>
+        <div class="collapse-title">
+          <i>${this.title}</i>
+        </div>
+      </div>
+      <iron-collapse .opened=${this.opened}>
+        <slot></slot>
+      </iron-collapse>
+    `;
+  }
+}
--- packages/collapse/src/oneup-collapse.ts
+++ packages/collapse/src/oneup-collapse.ts
@@ -1,81 +1,14 @@
-import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
-import "@polymer/iron-collapse/iron-collapse.js";
-import "@polymer/iron-icon/iron-icon.js";
-import "@polymer/iron-icons/hardware-icons.js";
-import "@aeb/aeb-shared-styles/aeb-shared-styles.js";
+import { customElement } from 'lit/decorators.js';
+import { OneupCollapseBase } from './oneup-collapse.base.js';
+import { styles } from './oneup-collapse.styles.js';
 
-class OneupCollapseElement extends PolymerElement {
-  static get template() {
-    return html`
-      <style include="aeb-shared-styles">
-        :host {
-          display: block;
-          margin: 20px 0;
-        }
-        iron-icon {
-          margin-right: 25px;
-          transition: all 0.2s ease-out;
-        }
-        .collapse-bar {
-          cursor: pointer;
-          background: #f8f8f8;
-          height: 64px;
-          display: flex;
-          align-items: center;
-          padding: 0 25px;
-          font-weight: bold;
-          border: 1px solid #f4f4f4;
-          user-select: none;
-        }
-        .collapse-title {
-          flex: 1;
-          text-align: center;
-          margin-left: -48px;
-        }
-        .collapsed-false {
-          transform: rotate(-90deg);
-        }
-        @media screen and (max-width: 767px) {
-          .collapse-bar {
-            height: 48px;
-            font-size: 14px;
-          }
-        }
-      </style>
-      <div class="collapse-bar" on-click="_toggle">
-        <iron-icon
-          class$="collapsed-[[opened]]"
-          icon="hardware:keyboard-arrow-down"
-        ></iron-icon>
-        <div class="collapse-title"><i>[[title]]</i></div>
-      </div>
-
-      <iron-collapse opened="[[opened]]">
-        <slot></slot>
-      </iron-collapse>
-    `;
-  }
-
-  static get properties() {
-    return {
-      title: {
-        type: String,
-        reflectToAttribute: true,
-      },
-      opened: {
-        type: Boolean,
-        reflectToAttribute: true,
-        notify: true,
-        value: false,
-      },
-    };
-  }
-
-  opened: any;
-
-  _toggle() {
-    this.opened = !this.opened;
+declare global {
+  interface HTMLElementTagNameMap {
+    'oneup-collapse': OneupCollapseBase;
   }
 }
 
-window.customElements.define("oneup-collapse", OneupCollapseElement);
+@customElement('oneup-collapse')
+export class OneupCollapse extends OneupCollapseBase {
+  static readonly styles = styles;
+}
