4bf39ccd119b493442064f400af6ee15a933e7ab
Leon Plata <pcg.leon@outlook.com>
2024-04-04 08:18:06 -0400

    Implemented monorepo

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import './album/album.js';
-import './collapse/collapse.js';
-import './drawer/drawer.js';
-import './layout/layout.js';
-import './lazy-image/lazy-image.js';
-import './link/link.js';
