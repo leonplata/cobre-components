5c46b68950cfcbee7de60f3170da77eb83bd43cf
Leon Plata <pcg.leon@outlook.com>
2024-04-04 12:39:25 -0400

    Added collapse

--- apps/stories/src/components/collapse.stories.ts
+++ apps/stories/src/components/collapse.stories.ts
@@ -1,24 +1,41 @@
 import { html } from 'lit'
 import type { Meta, StoryObj } from '@storybook/web-components';
-import type { IAlbumProps } from '@cobre/cobre-album';
-import '@cobre/cobre-album';
+import type { ICollapseProps } from '@cobre/cobre-collapse';
+import '@cobre/cobre-collapse';
 
 const meta = {
   title: 'Collapse',
-  render: (args: IAlbumProps) => html`
-    <cobre-album
-      .album=${args.album}
-      .slideshow=${args.slideshow ?? false}
-      slideshow-delay=${args.slideshowDelay ?? 10000}
+  render: (args: ICollapseProps) => html`
+    <cobre-collapse
+      .opened=${args.opened ?? false}
+      .title=${args.title ?? ''}
     >
-    </cobre-album>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim
+        vel dolor eu elementum. Nunc eget cursus urna, nec pretium nulla.
+        Integer efficitur tincidunt nibh a rhoncus. Curabitur interdum bibendum
+        purus, id vestibulum libero molestie a. Suspendisse in risus sed ipsum
+        euismod mollis. Quisque sagittis semper magna, vel facilisis sem semper
+        nec. Morbi tincidunt tortor non massa posuere, at rutrum mauris
+        ultrices. Quisque eget turpis est.
+      </p>
+    </cobre-collapse>
   `,
-} satisfies Meta<IAlbumProps>;
+} satisfies Meta<ICollapseProps>;
 
 export default meta;
 
-export const Default: StoryObj<IAlbumProps> = {
+export const Closed: StoryObj<ICollapseProps> = {
   args: {
-    album: []
+    opened: false,
+    title: 'Closed',
+  },
+};
+
+
+export const Opened: StoryObj<ICollapseProps> = {
+  args: {
+    opened: true,
+    title: 'Opened',
   },
 };
