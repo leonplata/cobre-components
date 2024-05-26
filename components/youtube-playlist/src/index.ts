2e860901ffec54666c545a378ef3cbff6c085f5f
Leon Plata <pcg.leon@outlook.com>
2024-05-26 07:14:49 -0400

    Exposing style on youtube-playlist

--- components/youtube-playlist/src/index.ts
+++ components/youtube-playlist/src/index.ts
@@ -1,4 +1,5 @@
-export { default } from './youtube-playlist'
-export * from './youtube-playlist'
-export * from './youtube-playlist.base'
-export * from './youtube-playlist.types'
+export { default } from './youtube-playlist';
+export * from './youtube-playlist';
+export * from './youtube-playlist.base';
+export * from './youtube-playlist.types';
+export { default as style } from './youtube-playlist.style.scss?inline';
